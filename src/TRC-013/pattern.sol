c59093ab3d3b7b3c840091162f299a6fb63b533b
beekeeper <allenfrom0517@gmail.com>
2024-07-25 15:11:42 +0800

    docs: update classification descriptions (#11)

--- src/TRC-013/pattern.sol
+++ src/TRC-013/pattern.sol
@@ -1,10 +1,8 @@
 function _transfer(address from, address recipient, uint256 amount) internal virtual override returns (bool) {
   require(_balances[_msgSender()] >= amount, );
+  require(_balances(from).sub(amount)>=1*10**18,);
   _balances[_msgSender()] -= amount;
-  uint256 fee = amount.mul(feeRate).div(100);
-  if (whitelist[msg.sender] == ture)
-  fee = 0;
-  _balances[recipient] += (amount-fee);
-  emit Transfer(_msgSender(), recipient, amount-fee);
+  _balances[recipient] += amount;
+  emit Transfer(_msgSender(), recipient, amount);
   return true;
 }
